ea0eac7b1392a0e69415b494bf3600708180006d
Brian E Hoch <brianhoch@me.com>
2023-04-04 20:25:28 -0500

    make colorize it's own module

---
+++ colorize.ts
@@ -0,0 +1,16 @@
+import * as colors from 'ansi-colors';
+
+export const bd = (txt) => colors.bold(txt);
+export const d = (txt) => colors.dim(txt);
+export const i = (txt) => colors.italic(txt);
+export const u = (txt) => colors.underline(txt);
+export const inv = (txt) => colors.inverse(txt);
+export const s = (txt) => colors.strikethrough(txt);
+export const r = (txt) => colors.red(txt);
+export const g = (txt) => colors.green(txt);
+export const y = (txt) => colors.yellow(txt);
+export const b = (txt) => colors.blue(txt);
+export const m = (txt) => colors.magenta(txt);
+export const c = (txt) => colors.cyan(txt);
+export const w = (txt) => colors.white(txt);
+export const gy = (txt) => colors.grey(txt);
--- pomodoro.ts
+++ pomodoro.ts
@@ -1,22 +1,8 @@
 import progress from 'cli-progress';
-import * as colors from 'ansi-colors';
 import { timers } from './timersTypes';
+import {
+    m
+} from './colorize';
 
-const bd = (txt) => colors.bold(txt);
-const d = (txt) => colors.dim(txt);
-const i = (txt) => colors.italic(txt);
-const u = (txt) => colors.underline(txt);
-const inv = (txt) => colors.inverse(txt);
-const s = (txt) => colors.strikethrough(txt);
-const r = (txt) => colors.red(txt);
-const g = (txt) => colors.green(txt);
-const y = (txt) => colors.yellow(txt);
-const b = (txt) => colors.blue(txt);
-const m = (txt) => colors.magenta(txt);
-const c = (txt) => colors.cyan(txt);
-const w = (txt) => colors.white(txt);
-const gy = (txt) => colors.grey(txt);
 
-console.log(bd(c('Hello')))
-
-console.log(JSON.stringify(timers.test, null, 2));
+console.log(m(JSON.stringify(timers.test, null, 2)));
